8dbe73121fe95bea3cc98589335f04c22913847d
mehmoodzafarfreelancing-ai <mehmoodzafarfreelancing@gmail.com>
2025-11-08 23:59:03 +0500

    Task Manager Starting

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 let savedText = "";
 
-$('#saveBTN').click(function() {
-    savedText
+const savedButton = document.getElementById('savedBtn');
+const savedTextDisplay = document.getElementById('savedText');
+const userTextField = document.getElementById('userText');
+ 
+savedButton.addEventListener('click', function() {
+    /* savedText = $("#userText").val();
+    $('#savedText').text(savedText); */
+    savedText = userTextField.value;
+    savedTextDisplay.textContent = savedText;
 });

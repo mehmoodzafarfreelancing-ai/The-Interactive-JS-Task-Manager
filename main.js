ef4fa537a7af0f34fe7be274aed1a8f32854a2a1
mehmoodzafarfreelancing-ai <mehmoodzafarfreelancing@gmail.com>
2025-11-10 22:43:52 +0500

    Making task manager good

--- main.js
+++ main.js
@@ -1,41 +1,76 @@
+const savedButton = document.getElementById("savedBtn");
+const savedTextDisplay = document.getElementById("savedText");
+const userTextField = document.getElementById("userText");
+const tasksKey = "myTasks";
+
+function loadTasks() {
+  const taskString = localStorage.getItem("tasksKey");
+  if (!taskString) return;
+  const taskArray = JSON.parse(taskString);
+  taskArray.forEach((task) => {
+    createTaskElement(task);
+  });
+}
+
+function saveTasks() {
+  let allTasks = [];
+  const lielements = document.querySelectorAll("#savedText li");
+  lielements.forEach((li) => {
+    const span = li.querySelector("span");
+    allTasks.push(span.textContent);
+    const allTasksString = JSON.stringify(allTasks);
+    localStorage.setItem("tasksKey", "allTasksString");
+  });
+}
 
 
-const savedButton = document.getElementById('savedBtn');
-const savedTextDisplay = document.getElementById('savedText');
-const userTextField = document.getElementById('userText');
-
-savedButton.addEventListener('click', function() {
-
-    /* save the entered value in let savedText */
-    const savedText = userTextField.value;
-
+/* @param {string} taskText  */
+function createTaskElement(taskText) {
     /* create a new li */
-    const newLi = document.createElement('li');
+  const newLi = document.createElement("li");
 
-    const taskText = document.createElement('span');
-    /* save the let savedText value in span of new li */
-    taskText.textContent = savedText;
-
-    /* create a new button and then change its name to Delete*/
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = "Delete";
-
-        /* function for deleting the li on clicking on delete */
-        deleteButton.addEventListener('click', function() {
-        newLi.remove();
-    });
+   const newSpan = document.createElement("span");
+   newSpan.textContent = taskText;
+}
 
 
 
-    newLi.appendChild(taskText);
-    /* add newly created button in li */
-    newLi.appendChild(deleteButton);
-
-        /* add newly created li (with saved value) in ul */
-    savedTextDisplay.appendChild(newLi);
-    /* set input field to empty */
-    userTextField.value = "";
 
 
+
+
+
+
+
+
+
+
+
+savedButton.addEventListener("click", function () {
+  /* save the entered value in let savedText */
+  const savedText = userTextField.value;
+
+
+/*   const newLi = document.createElement("li"); */
+
+
+
+  /* create a new button and then change its name to Delete*/
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+
+  /* function for deleting the li on clicking on delete */
+  deleteButton.addEventListener("click", function () {
+    newLi.remove();
+  });
+
+  newLi.appendChild(taskText);
+  /* add newly created button in li */
+  newLi.appendChild(deleteButton);
+
+  /* add newly created li (with saved value) in ul */
+  savedTextDisplay.appendChild(newLi);
+  /* set input field to empty */
+
+  userTextField.value = "";
 });
-
